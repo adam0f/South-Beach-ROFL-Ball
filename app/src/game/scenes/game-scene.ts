0d5433698a959d5eb474958aea07cb7d05a01314
adam0f <adam00ff00@gmail.com>
2021-09-02 01:03:18 -0400

    60 percent

--- app/src/game/scenes/game-scene.ts
+++ app/src/game/scenes/game-scene.ts
@@ -19,6 +19,13 @@ export class GameScene extends Phaser.Scene {
   private selectedGotchi?: AavegotchiGameObject;
   private balls?: Phaser.GameObjects.Group
   private crabs?: Phaser.GameObjects.Group
+  private score = 0
+  private scoreText?: Phaser.GameObjects.Text;
+  private ballCount = 3
+  private ballCountText?: Phaser.GameObjects.Text
+  private balldead = false
+  private toleranceLevel = 75
+  private tolText?: Phaser.GameObjects.Text
 
   // Sounds
   private back?: Phaser.Sound.BaseSound;
@@ -46,20 +53,20 @@ export class GameScene extends Phaser.Scene {
   }
 
   private ballGenerate = () => {
-  //  const dropLocation = 800
-  //  const dropAngle = 0
+    const dropLocation = 0
+    const dropAngle = 600
 
-  //PLACEMENT NEEDS TO BE STATIC or based on trait - kinship????
+    this.dropBall(dropLocation, dropAngle)
 
-    const placement = Math.floor(Math.random() * 6) + 1
-    const dropAngle = Math.floor(Math.random() * (getGameWidth(this) / 5))
-
-    for (let i = 0; i < 6; i++) {
-      if (i == placement ) {
-        const dropLocation = placement * (getGameWidth(this) / 6)
-        this.dropBall(dropLocation, dropAngle)
-      } 
-   }
+  // PLACEMENT NEEDS TO BE STATIC or based on trait - kinship????
+  //   const placement = Math.floor(Math.random() * 6) + 1
+  //   const dropAngle = Math.floor(Math.random() * (getGameWidth(this) / 5))
+  //   for (let i = 0; i < 6; i++) {
+  //     if (i == placement ) {
+  //       const dropLocation = placement * (getGameWidth(this) / 6)
+  //          this.dropBall(dropLocation, dropAngle)
+  //     } 
+  //  }
  }
 
   private dropBall = (dropLocation: number, dropAngle: number): void => {
@@ -73,19 +80,31 @@ export class GameScene extends Phaser.Scene {
     this.add.image(getGameWidth(this) / 2, getGameHeight(this) / 2, BG).setDisplaySize(getGameWidth(this), getGameHeight(this));
     this.back = this.sound.add(CLICK, { loop: false });
     this.createBackButton();
+    this.scoreText = this.add.text(getGameWidth(this) * 0.5, getGameHeight(this) * 0.15, this.score.toString(), { color: '0x005500'}).setFontSize(getRelative(94, this)).setOrigin(0.5).setDepth(1)
+    this.ballCountText = this.add.text(getGameWidth(this) * 0.2, getGameHeight(this) * 0.15, this.ballCount.toString(), { color: '0x000000'}).setFontSize(getRelative(94, this)).setOrigin(0.5).setDepth(1)
+    this.tolText = this.add.text(getGameWidth(this) * 0.8, getGameHeight(this) * 0.15, this.ballCount.toString(), { color: '0x000000'}).setFontSize(getRelative(94, this)).setOrigin(0.5).setDepth(1)
 
-    const bottom = this.add.sprite(getGameWidth(this)/2 , getGameHeight(this) *.8125 + getGameHeight(this) / 5.3333, BOTTOM).setDisplaySize(getGameWidth(this), getGameHeight(this) / 5.3333)  
+    const bottom = this.add.sprite(getGameWidth(this)/2 , getGameHeight(this) * 0.75 + getGameHeight(this) / 5.3333, BOTTOM).setDisplaySize(getGameWidth(this), getGameHeight(this) / 5.3333)  
     this.physics.add.existing(bottom, true)
 
+    const floor = this.add.rectangle(0, getGameHeight(this) * 0.9).setDisplaySize(getGameWidth(this), 50).setOrigin(0, 0)
+    this.physics.add.existing(floor, true)
+
+    const leftWall = this.add.rectangle(-100, -getGameHeight(this) * 40, 0x000000).setDisplaySize(50, getGameHeight(this) * 50,).setOrigin(0, 0)
+    this.physics.add.existing(leftWall, true)
+
+    const rightWall = this.add.rectangle(getGameWidth(this), -getGameHeight(this) * 40, 0x000000).setDisplaySize(50, getGameHeight(this) * 50,).setOrigin(0, 0)
+    this.physics.add.existing(rightWall, true)
+
     this.crabs = this.add.group({
-      maxSize: 100,
+      maxSize: 500,
       classType: Crab,
     })
 
     this.getCrab()
 
     this.time.addEvent({
-      delay: 2000,
+      delay: 1000,
       callback: this.getCrab,
       callbackScope: this, 
       loop: true,
@@ -94,16 +113,16 @@ export class GameScene extends Phaser.Scene {
     this.balls = this.add.group({
       maxSize: 100,
       classType: Ball,
-    })
+    })   
 
-    this.ballGenerate()
+    this.ballGenerate()    
 
-    this.time.addEvent({
-      delay: 2000,
-      callback: this.ballGenerate, 
-      callbackScope: this,
-      loop: true,
-    })
+    // this.time.addEvent({
+    //   delay: 2000,
+    //   callback: this.ballGenerate, 
+    //   callbackScope: this,
+    //   loop: true,
+    // })
    
 
     // Add a player sprite that can be moved around.
@@ -114,11 +133,45 @@ export class GameScene extends Phaser.Scene {
       key: this.selectedGotchi?.spritesheetKey || ''
     })
   
+    this.physics.add.collider(bottom, this.player);
+    this.physics.add.collider(floor, this.balls);
+    this.physics.add.collider(leftWall, this.balls);
+    this.physics.add.collider(rightWall, this.balls);
+    this.physics.add.collider(this.player,this.balls, () => { this.addScore() });
+    this.physics.add.collider(this.balls, this.crabs, () => { this.cycleBall() });
+    this.physics.add.overlap(this.player, this.crabs, () => { this.lowerTolerance() });
+  }
 
-    this.physics.add.collider(bottom, this.player)
-    this.physics.add.collider(this.balls,this.player)
-    this.physics.add.collider(this.balls, this.crabs, function(ball) { ball.destroy() })
+  private lowerTolerance() {
+    if (this.toleranceLevel > 0) {
+      this.toleranceLevel--;
+      this.crabs?.setVisible(false)
+         Phaser.Actions.Call((this.crabs as Phaser.GameObjects.Group).getChildren(), (crab) => { 
+          (crab.body as Phaser.Physics.Arcade.Body).destroy()
+          },
+          this,);
+          this.tolText?.setText(this.toleranceLevel.toString())
+    }
 
+  }
+
+  private cycleBall() {
+    if (this.ballCount > 0) {
+      this.ballCount--;
+      this.balls?.setVisible(false)
+      // Phaser.Actions.Call((this.balls as Phaser.GameObjects.Group).getChildren(), (ball) => { 
+      //   (ball.body as Phaser.Physics.Arcade.Body).destroy();
+      //   },
+      // this,)
+      this.ballCountText?.setText(this.ballCount.toString())
+    } else {  
+      window.history.back();
+    }
+  }
+
+  private addScore() {
+    this.score += 1
+    this.scoreText?.setText(this.score.toString())
   }
 
   private createBackButton = () => {
@@ -132,10 +185,9 @@ export class GameScene extends Phaser.Scene {
         window.history.back();
       });
   };
-
-  //add offscreen desrtoy
+  
   public update(): void {
     // Every frame, we update the player
-    this.player?.update();
+    this.player?.update();    
   }
 }
